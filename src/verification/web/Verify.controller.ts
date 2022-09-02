831ea3c599722e20e008ef9a7edd06f5c3a68062
Sadhan Sarker <cse.sadhan@gmail.com>
2022-09-02 21:23:43 +0600

    replace old path import

--- src/verification/web/Verify.controller.ts
+++ src/verification/web/Verify.controller.ts
@@ -3,7 +3,7 @@ import {StatusCodes} from "http-status-codes";
 import {Request, Response, Router} from "express";
 import {VerificationReq} from "../dto/VerificationReq";
 import {VerificationRes} from "../dto/VerificationRes";
-import {Method} from "../../common/Method.enum";
+import {Method} from "~/common/Method.enum";
 import {SRouter, SRequestMapping} from "~/common/SRequestMapping";
 
 
